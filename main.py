bc849b5725c5d280e5361aa913e973a15d1f4aa6
ben <bnharel@gmail.com>
2023-11-27 19:15:17 +0200

    learning python

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-name = input("Your name: ")
-family_name = input("Your family name: ")
+import numpy as np
+import test
 
-age = input("enter your age: ")
-print(name)
+my_list = [1, 2, "3"]
 
+my_tuple = (2, 2, 4)
+
+my_list[0] = 8
+
+dict = {"sagi" : "or", 
+        "yaron" : "drukh", 
+        "ben" : "harel"}
+
+for item in my_list:
+    print(item, type(item))
+    
+if (my_list[0] == 8):
+    print("hello")
+
+
+def addition(x, y):
+    return x + y
+
+print(addition(2, 3))
+
+print(len(my_list))
